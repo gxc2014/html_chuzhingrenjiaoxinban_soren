0e3ef61d159e5fe033e5c3c9d1384922ab54dd62
gxc2014 <gxc20140514@outlook.com>
2026-07-28 21:50:28 +0800

    Update service worker to use new cache version

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,65 @@
-// 简单的 Service Worker，缓存当前页面以便离线使用
-const CACHE_NAME = 'gushi-v1';
+const CACHE_NAME = 'gushi-v2';
+
 const urlsToCache = [
-  'html_chuzhingrenjiaoxinban_soren.html',
-  'manifest.json',
-  // 如果有其他资源（CSS/JS/图标）也加入缓存
-  'icons.png'
+  './html_chuzhingrenjiaoxinban_soren.html',
+  './manifest.json',
+  './icons.png'
 ];
 
+
+// 安装
 self.addEventListener('install', event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+
+// 激活，删除旧缓存
+self.addEventListener('activate', event => {
+
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if(key !== CACHE_NAME){
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
+
+  self.clients.claim();
+
+});
+
+
+// 网络优先，失败使用缓存
+self.addEventListener('fetch', event => {
+
+  event.respondWith(
+
+    fetch(event.request)
+      .then(response => {
+
+        let clone = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, clone);
+          });
+
+        return response;
+
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+
+  );
+
 });
